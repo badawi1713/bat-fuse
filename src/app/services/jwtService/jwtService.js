38ccad057bb644e197b668d30105f6482a27833a
badawi1713 <dzaky.badawi@gmail.com>
2020-12-28 23:20:02 +0700

    Change endpoint value and caption for Tenayan Unit 2

--- src/app/services/jwtService/jwtService.js
+++ src/app/services/jwtService/jwtService.js
@@ -62,7 +62,7 @@ class JwtService extends FuseUtils.EventEmitter {
 	signInWithEmailAndPassword = (username, password) => {
 		return new Promise((resolve, reject) => {
 			axios
-				.post(`http://10.7.1.110:8083/service/bat/auth`, {
+				.post(`http://10.7.1.110:8084/service/bat/auth`, {
 					data: {
 						username,
 						password
@@ -102,7 +102,7 @@ class JwtService extends FuseUtils.EventEmitter {
 	signInWithToken = () => {
 		return new Promise((resolve, reject) => {
 			axios
-				.get(`http://10.7.1.110:8083/service/bat/validate/access-token`, {
+				.get(`http://10.7.1.110:8084/service/bat/validate/access-token`, {
 					headers: {
 						Authorization: `Bearer ${this.getAccessToken()}`,
 						Accept: 'application/json',
