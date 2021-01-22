0343f310d81ba1339a7f5e89d1516e279fea5093
badawi1713 <dzaky.badawi@gmail.com>
2021-01-22 09:24:42 +0700

    Change chart title in home page

--- src/app/main/Home/index.jsx
+++ src/app/main/Home/index.jsx
@@ -40,8 +40,8 @@ const Home = () => {
 							<Paper square className="border-grey-800 bg-transparent border w-full h-full rounded p-8">
 								<Grid container className="w-full h-full">
 									<Grid item className="w-4/5 h-full mr-8 flex flex-col justify-center">
-										<Typography className="text-center uppercase">
-											BAT Efficiency Improvement Dashboard
+										<Typography className="text-center text-16 uppercase">
+											Boiler Efficiency Improvement
 										</Typography>
 										<Chart />
 									</Grid>
@@ -52,7 +52,7 @@ const Home = () => {
 													Current Efficiency
 												</Typography>
 												<Typography className=" text-center text-36 text-light-green-A700 font-semibold">
-													73%
+													73.9%
 												</Typography>
 												<div />
 											</Paper>
